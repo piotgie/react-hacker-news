361cc8085daecb6a3ac3fa41579bbd9f49d982d6
Piotr Gaik <piotgie@gmail.com>
2021-12-30 22:37:09 +0100

    init

---
+++ src/Stories.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { useGlobalContext } from './context'
+
+const Stories = () => {
+
+  const { isLoading, hits, removeStory } = useGlobalContext();
+
+  if (isLoading) {
+    return <div className="loading"></div>
+  }
+
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        const { objectID, title, num_comments, url, points, author } = story
+        return (
+          <article key={objectID} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="inf">{points} points by <span>{author} | </span> {num_comments} {'  '} comments</p>
+            <div>
+              <a className="read-link" href={url} rel="noopener noreferrer" target='_blank'>
+                read more
+              </a>
+              <button className="remove-btn" onClick={() => removeStory(objectID)}>remove</button>
+            </div>
+          </article>
+        )
+      })}
+    </section>
+  )
+}
+
+export default Stories
